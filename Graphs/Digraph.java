06dedcb68a99db804cf299092a54c80aaf6835a6
DM Coulter <dcoult01@seattlecentral.edu>
2018-03-22 08:59:39 -0700

    Add files via upload

---
+++ Graphs/Digraph.java
@@ -0,0 +1,62 @@
+package program7;
+
+/*
+ * Name:			Danielle Coulter
+ * Class:       AD 325 WI 18
+ * Assignment:  8
+ * Title:       Digraph.java 
+ * Due Date:    March 10, 2018
+ * Material:		Graphs: 4.1 Undirected & 4.2 Directed 
+ * Reference:	https://algs4.cs.princeton.edu/42digraph/Digraph.java.html
+ */
+
+import java.util.Scanner;
+
+public class Digraph {
+	private final int V; //number of vertices
+	private int E;       //number of edges
+	private Bag<Integer>[] adj; //adjaceny list using Bag
+
+	public Digraph(int V) {//creates array of empty list
+		this.V = V;
+		this.E = 0;
+		adj = (Bag<Integer>[]) new Bag[V];
+		for (int v = 0; v < V; v++)
+			adj[v] = new Bag<Integer>();
+	}
+
+	public Digraph(Scanner input) {
+		this(input.nextInt());    //first number in file is V
+		int N = input.nextInt();  //second number in file is number of edges
+		for(int i = 0; i < N; i++) {
+			int n1 = input.nextInt(); //read first column vertex
+			int n2 = input.nextInt(); //read second column vertex
+			addEdge(n1, n2);
+		}
+	}
+	
+	public int V() {
+		return V;
+	}
+
+	public int E() {
+		return E;
+	}
+
+	public void addEdge(int v, int w) {
+		adj[v].add(w);//this will add w to v's list
+		E++;//increase E number
+	}
+
+	public Iterable<Integer> adj(int v) {
+		return adj[v];
+	}
+
+	public Digraph reverse() {
+		Digraph R = new Digraph(V);
+		for (int v = 0; v < V; v++)
+			for (int w : adj(v))
+				R.addEdge(w, v);
+		return R;
+	}
+}
